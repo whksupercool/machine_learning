c3a2bbd012224a557b833142e586547926c28ac4
Mr.wang <1225611688@qq.com>
2020-10-17 18:07:26 +0800

    机器学习提交git管理 2020-10-17 18:07

---
+++ project01/demo01字典特征数据抽取.py
@@ -0,0 +1,86 @@
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+import jieba
+
+
+def dictvec():
+    """
+    字典数据抽取
+    :return: None
+    """
+    # 实例化
+    dict = DictVectorizer(sparse=False)
+
+    # 调用fit_transform
+    data = dict.fit_transform(
+        [{'city': '北京', 'temperature': 100}, {'city': '上海', 'temperature': 60}, {'city': '深圳', 'temperature': 30}])
+
+    print(dict.get_feature_names())
+
+    print(dict.inverse_transform(data))
+
+    print(data)
+
+    return None
+
+
+def countvec():
+    """
+    对文本进行特征值化
+    :return: None
+    """
+    cv = CountVectorizer()
+    data = cv.fit_transform(["人生苦短，我们 喜欢 python", "人生漫长，我们 不用 python"])
+
+    print(cv.get_feature_names())
+
+    print(data.toarray())
+
+    return None
+
+
+def cut_word():
+    """
+    分词器
+    :return: 
+    """
+
+    con1 = jieba.cut("今天很残酷，明天更残酷，后天很美好，但绝对大部分是死在明天晚上，所以每个人不要放弃今天。")
+    con2 = jieba.cut("我们看到的从很远星系来的光是在几百万年之前发出的，这样当我们看到宇宙时，我们是在看它的过去。")
+    con3 = jieba.cut("如果只用一种方式了解某样事物，你就不会真正了解它。了解事物真正含义的秘密取决于如何将其与我们所了解的事物相联系。")
+
+    # 转换成列表
+    content1 = list(con1)
+    content2 = list(con2)
+    content3 = list(con3)
+
+    # 把列表转换成字符串
+    c1 = ' '.join(content1)
+    c2 = ' '.join(content2)
+    c3 = ' '.join(content3)
+
+    return c1, c2, c3
+
+
+def chinese_characters_vec():
+    """
+    中文特征值化
+    :return: None
+    """
+
+    c1, c2, c3 = cut_word()
+
+    cv = CountVectorizer()
+    data = cv.fit_transform([c1, c2, c3])
+
+    print(cv.get_feature_names())
+
+    print(data.toarray())
+
+    return None
+
+
+if __name__ == '__main__':
+    # dictvec()
+    # countvec()
+    chinese_characters_vec()
---
+++ project01/demo02文本特征数据抽取.py
@@ -0,0 +1,17 @@
+from sklearn.feature_extraction.text import CountVectorizer
+
+data = ["life is short,i like python","life is too long,i dislike python"]
+
+def dictvec():
+    """
+    字典数据特征抽取
+    :return:None 
+    """
+    # 实例化
+
+
+    return None
+
+
+if __name__ == '__main__':
+    dictvec()
