9b9c2e8428e4aacf452b51db48db847181c34fe0
Mr.wang <1225611688@qq.com>
2020-10-17 23:48:18 +0800

    机器学习提交git管理 2020-10-17 23:48

--- project01/demo01字典特征数据抽取.py
+++ project01/demo01字典特征数据抽取.py
@@ -1,5 +1,8 @@
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.impute import SimpleImputer  # 自版本0.20以来 SimpleImputer 代替 Imputer
+import numpy as np
 import jieba
 
 
@@ -80,7 +83,79 @@ def chinese_characters_vec():
     return None
 
 
+def tf_idf_vec():
+    """
+    中文特征值化(频率)
+    :return: None
+    """
+
+    c1, c2, c3 = cut_word()
+
+    tf = TfidfVectorizer()
+    data = tf.fit_transform([c1, c2, c3])
+
+    print(tf.get_feature_names())
+
+    print(data.toarray())
+
+    return None
+
+
+def mm():
+    """
+    归一化处理
+    :return: None
+    """
+
+    mm = MinMaxScaler()
+    data = mm.fit_transform([[90, 2, 10, 40], [60, 4, 15, 45], [75, 3, 13, 46]])
+    print(data)
+
+    return None
+
+
+def im():
+    """
+    缺失值处理
+    :return: None
+    """
+
+    im = SimpleImputer(missing_values='nan', strategy='mean')
+
+    data = im.fit_transform([[1, 2], [np.nan, 3], [7, 6]])
+
+    print(data)
+
+    return None
+
+
+def stand():
+    """
+    标准化缩放
+    :return: None
+    """
+    std = StandardScaler()
+    # data = std.fit_transform([[1., -1., 3.], [2., 4., 2.], [4., 6., -1.]])
+    data = std.fit_transform([[14488, 7.153469, 1.673904],
+                              [26052, 1.441871, 0.805124],
+                              [75136, 13.147394, 0.428964],
+                              [38344, 1.669788, 0.134296],
+                              [72993, 10.141740, 1.032955],
+                              [35948, 6.830792, 1.213192],
+                              [42666, 13.276369, 0.543880],
+                              [67497, 8.631577, 0.749278],
+                              [35483, 12.273169, 1.508053],
+                              [50242, 3.723498, 0.831917]])
+    print(data)
+
+    return None
+
+
 if __name__ == '__main__':
     # dictvec()
     # countvec()
-    chinese_characters_vec()
+    # chinese_characters_vec()
+    # tf_idf_vec()
+    # mm()
+    # stand()
+    im()
---
+++ project01/demo03归一化.py
@@ -0,0 +1,20 @@
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+
+data = ["life is short,i like python", "life is too long,i dislike python"]
+
+
+def dictvec():
+    """
+    字典数据特征抽取
+    :return:None 
+    """
+    # 实例化
+
+    return None
+
+
+if __name__ == '__main__':
+    dictvec()
+
+
