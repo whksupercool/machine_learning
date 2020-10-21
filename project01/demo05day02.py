34cbbddf42f96afcb5beab7566ea7a50e0569092
Mr.wang <1225611688@qq.com>
2020-10-21 20:23:29 +0800

    机器学习提交git管理 2020-10-21 20:23

--- project01/demo05day02.py
+++ project01/demo05day02.py
@@ -1,6 +1,10 @@
+from sklearn.datasets import load_iris, fetch_20newsgroups, load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
 import pandas as pd
 
 
@@ -36,11 +40,12 @@ def knncls():
     place_count = data.groupby("place_id").count()
     tf = place_count[place_count.row_id > 3].reset_index()  # reset_index() 重新设置索引，把place_id设置为一列数据
     data = data[data["place_id"].isin(tf.place_id)]
-    # print(data)
+    print(data)
 
     # 取出数据当中的目标值（y）和特征值（x）
     y = data["place_id"]
-    x = data.drop(["place_id"], axis=1)
+    # x = data.drop(["place_id"], axis=1)
+    x = data.drop(["row_id"], axis=1)
 
     # 进行数据分割的数据集和测试集
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
@@ -51,7 +56,6 @@ def knncls():
     # 对测试集和训练集的特征值进行标准化
     x_train = std.fit_transform(x_train)
     x_test = std.fit_transform(x_test)
-
 
     # 进行算法流程
     knn = KNeighborsClassifier(n_neighbors=5)
@@ -69,5 +73,39 @@ def knncls():
     return None
 
 
+def naviebayes():
+    """
+    朴素贝叶斯进行文本分类
+    :return: None
+    """
+    news = fetch_20newsgroups(subset="all")
+
+    # 进行数据分割
+    x_train, x_test, y_train, y_test = train_test_split(news.data, news.target, test_size=0.25)
+
+    # 对数据集进行特征抽取
+    tf = TfidfVectorizer()
+    # 以训练集当中的词的列表进行每篇文章重要性统计
+    x_train = tf.fit_transform(x_train)
+    # print(tf.get_feature_names())
+    x_test = tf.transform(x_test)
+
+    # 进行朴素贝叶斯算法的预测
+    mlt = MultinomialNB(alpha=1.0)
+    # print(x_train.toarray())
+    mlt.fit(x_train, y_train)
+    y_predict = mlt.predict(x_test)
+    print("预测的文章类别为：", y_predict)
+
+    # 得出准确率
+    print("准确率为：", mlt.score(x_test, y_test))
+
+    # 得出精确率和召回率
+    print("每个类别的精确率和召回率：", classification_report(y_test, y_predict, target_names=news.target_names))
+
+    return None
+
+
 if __name__ == '__main__':
-    knncls()
+    # knncls()
+    naviebayes()
